dcd8ea8eab27da52d401b7e6bb1a8d2945ddba0a
Chris Cooke <koodhini@gmail.com>
2017-05-01 21:25:01 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>
+<html>
+<head>
+<meta http-equiv='refresh' content='2;url=<?php echo $actual_link; ?>'>
+<?php
+echo 'magento on EB <br/>';
+
+echo 'Looking in the media folder...';
+
+if ($handle = opendir('var/media')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
+
+?>
